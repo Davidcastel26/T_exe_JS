1475ab97e3ac18cdefd73f31694fd3a2827a2851
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-15 09:24:42 -0600

    making the aprox

---
+++ subsetSum.js
@@ -0,0 +1,27 @@
+
+const subsetSum = (nums, n) =>{
+
+ const suma = [0]
+
+ 
+
+}
+
+
+/**
+ * aproximacion
+ * recorrer el arreglo preguntando si ya encontre el target
+ * variable acomuladora para ver si coincide con el target
+ * restarle al target con lo que haya en el arr y sii da 0 es porque tengo suma
+ * validacion matematicas, para considerar todos los casos
+ */
+
+/*
+
+const arr = [1,2,3,4,5]
+
+const func = (e) => e % 2 === 0
+
+arr.some((e)=> e % 2 === 0)      -> true
+
+*/
