869c192508f029c5799c436cb87b884acefbf445
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-15 09:18:32 -0600

    clockminute

---
+++ clockMinute.js
@@ -0,0 +1,18 @@
+function clockMitueAdder(time,minutesToAdd){
+
+    if(minutesToAdd === 0) return time;
+    
+
+}
+
+/*
+
+-manejar todo con numeros
+-separar la hora de los minutos
+-el segundo parametro es mayor a 60? pregunto para dividirlo, sumar la hora, ver cuanto resta
+-considerar pasarlo a formato fecha para ver que logica podriamos aplicar 24hrs
+-sumar minutos a los minutos anteriores y volver a preguntar si es +60
+-dividir min entre 60 para calcular las horas
+-preguntar si el 2do param es 0, si es, devuelvo el 1re param
+
+*/
