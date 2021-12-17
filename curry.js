22d07a8f3ea3b0ffe5ee93fdc0bf4a445d053453
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-17 13:46:07 -0600

    adding second option es6

--- curry.js
+++ curry.js
@@ -1,12 +1,8 @@
 function curry(fn){
 
-    return function sepparador(){
-        //acumulador de argumentos
-        var args= [].slice().call(arguments)
-        //call -- arguments (local var)
-        if(args.length >= fn.length){
-            return fn.apply(null, args)
-        }
+    return function seraparador(...args) { 
+        if(args.length >= fn.length) return fn(...args)
+        else return (...newArgs)=>seraparador(...args, ...newArgs)
     }
 
 }
@@ -18,4 +14,22 @@ function curry(fn){
  -verificar cuantos arg tiene la fn, e ir comparando con los acumulados
  -separar los elemento del array
  -ir sacando 
+
+return function sepparador(){
+        //acumulador de argumentos
+        var args= [].slice.call(arguments)
+        //call -- arguments (local var)
+        if(args.length >= fn.length){
+            return fn.apply(null, args)
+        }else {
+            return function resolver(){
+                return sepparador.apply(null, args.concat([].slice.call(arguments)))
+            }
+        }
+    }
+
+
+
+
+
  */
