eff6ed8686b3a2b26706ec8b3e74e96a0380742c
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-19 21:44:36 -0600

    Update linear_search.js

--- Js/linear_search.js
+++ Js/linear_search.js
@@ -1,11 +1,26 @@
 
-function lenear_search(arrai, target){
+function linear_search(arrai, target){
     // Return the index position of the target if found, else returns None
     for( let i = 0; arrai.length < 0; i++){
         if(arrai[i] == target ){
             return i
         }
-        return 
+        return null
     }
-
 }
+
+function verify(index){
+    if( index != null){
+        console.log("Target found at index: ", index );
+    }else{
+        console.log("Target not found in the List")
+    }   
+}
+
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+
+let result = linear_search(numbers, 12);
+verify(result)
+
+let result2 = linear_search(numbers, 8);
+verify(result2)
