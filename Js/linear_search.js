ca316fd2a624d620ecf713c91405d49ee610a50c
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-19 21:37:35 -0600

    Create linear_search.js

---
+++ Js/linear_search.js
@@ -0,0 +1,11 @@
+
+function lenear_search(arrai, target){
+    // Return the index position of the target if found, else returns None
+    for( let i = 0; arrai.length < 0; i++){
+        if(arrai[i] == target ){
+            return i
+        }
+        return 
+    }
+
+}
