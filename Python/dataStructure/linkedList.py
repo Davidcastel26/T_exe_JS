cde310b9d713f5ce43297e482d609addad1a01f6
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-18 21:31:41 -0600

    working on linkedList.py

---
+++ Python/dataStructure/linkedList.py
@@ -0,0 +1,10 @@
+class Node:
+    """
+    An objecty for storing a single node of a linked list.
+    Models tow attributes - data and the link to the next node in the list
+    """
+    data = None
+    next_node = None
+
+def __init__(self, data):
+    self.data = data
