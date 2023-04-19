0d9ad0ee22faf7e8fb6dbc520ea1339f9cf3dec3
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-18 21:37:01 -0600

    working on linkedList.py

--- Python/dataStructure/linkedList.py
+++ Python/dataStructure/linkedList.py
@@ -6,5 +6,14 @@ class Node:
     data = None
     next_node = None
 
-def __init__(self, data):
-    self.data = data
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+class LinkedList:
+    head = None
+
+    def __init__(self):
+        self.head = None
