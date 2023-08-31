faff3cf8ef62212727b96c05aaf1c7d57fc9c2ab
David Castellanos <davcastellanoslarios@gmail.com>
2023-08-30 21:04:47 -0600

    working with canContruct js

---
+++ Js/canContruct.js
@@ -0,0 +1,4 @@
+//write a function `canContruct(target, wordBank` that accepts a target string and an array of strings
+// the function should return a boolean indicating whether or not the `target` can be contructed by concatenating elements ßof the `wordBank` array.
+// You may reuse elements of `wordbank` as many times as needed.
+
