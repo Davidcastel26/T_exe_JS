d81a484ba8d73beda54dbb96c14969129100f84e
David Castellanos <davcastellanoslarios@gmail.com>
2023-08-31 09:37:12 -0600

    done with memonization

--- Js/canContruct.js
+++ Js/canContruct.js
@@ -2,3 +2,25 @@
 // the function should return a boolean indicating whether or not the `target` can be contructed by concatenating elements ßof the `wordBank` array.
 // You may reuse elements of `wordbank` as many times as needed.
 
+
+const canConstruct = ( target, wordbank, memo = {} ) => {
+    if(target in memo) return memo[target]
+    if(target === '') { return true }
+  
+    for(let word of wordbank){
+      if(target.indexOf(word) === 0){
+        const suffix = target.slice(word.length)
+        if(canConstruct(suffix, wordbank, memo) === true ){
+          memo[target] = true
+          return true
+        }
+      }
+    }
+  
+    memo[target]= false
+    return false
+}
+
+canConstruct("abcdef",["ab","abc","cd","def","abcd"])
+canConstruct("skateboard",["bo","rd","ate","t","ska","sk","boar"])
+canConstruct("eeeeeeeeeeeeeeeeeeef",["e","ee","eee","eeee","eeeee","eeeeee"])
