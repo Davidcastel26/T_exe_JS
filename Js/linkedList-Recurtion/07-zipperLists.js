8aa608a10d8a1093c8e36d8a2afd118583d025e9
David Castellanos <davcastellanoslarios@gmail.com>
2023-09-18 14:01:37 -0600

    binary code

---
+++ Js/linkedList-Recurtion/07-zipperLists.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor(val){
+      this.val = val;
+      this.next = null
+    }
+  }
+  
+  const a = new Node("a");
+  const b = new Node("b");
+  const c = new Node("c");
+  const d = new Node("d");
+  
+  a.next = b;
+  b.next = c;
+  c.next = d;
+  
+  const x = new Node("x");
+  const y = new Node("y");
+  const z = new Node("z");
+  
+  x.next = y;
+  y.next = z;
+  
+  export const zipperList = (head1, head2) => {
+    
+   if(head1 === null && head2 === null) return null;
+   if(head1 === null ) return head2;
+   if(head2 === null ) return head1;
+    
+    const next1 = head1.next;
+    const next2 = head2.next;
+    head1.next = head2;
+    head2.next = zipperList(next1, next2);
+    return head1
+  }
+  
+  
+  zipperList(a, x)
+  
+  
+  
