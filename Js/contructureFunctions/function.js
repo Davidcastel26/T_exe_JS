d3519c9da105a6addafd12f9268100427b5f5112
David Castellanos <davcastellanoslarios@gmail.com>
2023-09-29 09:16:04 -0600

    constructor functions

---
+++ Js/contructureFunctions/function.js
@@ -0,0 +1,12 @@
+function  User(firstName, lastName, age, gender){
+    this.firstName = firstName;
+    this.lastName =  lastName;
+    this.age=  age;
+    this.gender = gender;
+}
+
+var user1 = new User('Dave','L', 26, 'male');
+var user2 = new User('Jill','Robinson', 25,'female')
+
+user1;
+user2;
