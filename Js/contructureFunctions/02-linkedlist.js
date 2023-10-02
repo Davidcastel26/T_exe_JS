bf12208c1daa7c597fe76edc66ee5a2a5850c8b6
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-02 17:18:54 -0600

    linked list with proto types

--- Js/contructureFunctions/02-linkedlist.js
+++ Js/contructureFunctions/02-linkedlist.js
@@ -1,39 +1,39 @@
 function LinkedList() {
     this.head = null;
     this.tail = null;
-  }
-  
-  function Node(value, next, prev){
+}
+
+function Node(value, next, prev){
     this.value = value;
     this.next = next;
     this.prev = prev;
-  }
+}
   
-  LinkedList.prototype.__addToHead__ = function(value) {
+LinkedList.prototype.__addToHead__ = function(value) {
     var newNode = new Node(value, this.head, null);
     if(this.head) this.head.prev = newNode;
     else this.tail = newNode;
     this.head = newNode;
-  }
+}
   
-  LinkedList.prototype.__addToTail__ = function( value ){
+LinkedList.prototype.__addToTail__ = function( value ){
     var newNode = new Node( value, null, this.tail );
     if( this.tail ) this.tail.next = newNode;
     else this.head = newNode;
     
     this.tail = newNode;
-  }
+}
   
-  LinkedList.prototype.__removeHead__ = function(){
+LinkedList.prototype.__removeHead__ = function(){
     if( !this.head ) return null;
     var val = this.head.value;
     this.head = this.head.next;
     if(this.head) this.head.prev = null;
     else this.tail = null;
     return val
-  }
+}
 
-  LinkedList.prototype.__removeTail__ = function(){
+LinkedList.prototype.__removeTail__ = function(){
     if( !this.tail ) return null;
     var val = this.tail.value;
     this.tail = this.tail.prev;
@@ -41,9 +41,9 @@ function LinkedList() {
     else this.head = null;
     
     return val;
-  }
+}
   
-  LinkedList.prototype.__search__ = function(searchValue) {
+LinkedList.prototype.__search__ = function(searchValue) {
     var currentNode  = this.head;
     
     while( currentNode ){
@@ -52,11 +52,11 @@ function LinkedList() {
       
     }
     return null;
-  }
+}
 
-  var myLL = new LinkedList();
+var myLL = new LinkedList();
   myLL.__addToHead__(1000);
   myLL.__addToHead__(2000);
   myLL.__addToTail__(3000);
   
-  console.log(myLL.__removeHead__())
+console.log(myLL.__removeHead__())
