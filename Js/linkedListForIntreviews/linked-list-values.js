73473cd72cf43379add22dc39433954e57e937a9
Davidcastel26 <davcastellanoslarios@gmail.com>
2023-12-29 15:48:21 -0600

    Update linked-list-values.js

--- Js/linkedListForIntreviews/linked-list-values.js
+++ Js/linkedListForIntreviews/linked-list-values.js
@@ -14,11 +14,39 @@ a.next = b;
 b.next = c;
 c.next = d;
 
-const printList = ( head ) => {
-    let current = head;
-    while( current !== null ){
-        console.log(current.value)
-    }
+// const printList = ( head ) => {
+    // let current = head;
+    // while( current !== null ){
+    //     console.log(current.value)
+    // }
+// }
+
+// const printList = ( head ) => {
+//     if( head === null ) return;
+//     console.log(head.val)
+//     printList(head.next)
+// }
+ 
+// printList(a)\
+
+
+// const linkedlistValues = ( head ) => {
+//     const values = [];
+//     let current = head;
+//     while( current !== null){
+//         values.push(current.val)
+//         current = current.next
+//     }
+// }
+
+const linkedlistValues = ( head ) => {
+    const values = [];
+    fillValues(head, values);
+    return values;
 }
 
-printList(a)
+const fillValues = ( head, values )=> {
+    if( head === null) return;
+    values.push(head.val)
+    fillValues(head.next, values)
+}
