b16798c3086902def55b7fa2dc3c44b403d21433
Davidcastel26 <davcastellanoslarios@gmail.com>
2023-12-29 16:15:47 -0600

    Update sumList.js

--- Js/linkedListForIntreviews/sumList.js
+++ Js/linkedListForIntreviews/sumList.js
@@ -6,13 +6,18 @@ class Node {
 }
 
 
-const sumList = ( head ) => {
-    let sum = 0
-    let current = head;
+// const sumList = ( head ) => {
+//     let sum = 0
+//     let current = head;
 
-    while( current !== null ){
-        sum += current.val;
-        current = current.next;
-    }
-    return sum
+//     while( current !== null ){
+//         sum += current.val;
+//         current = current.next;
+//     }
+//     return sum
+// }
+
+const sumList = ( head ) => {
+    if( head === null ) return 0;
+    return head.val + sumList(head.next)
 }
